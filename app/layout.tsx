c9372940711252d53d6d3f2c721effa73064bfa3
Aman Panwar <amanpanwar1672@gmail.com>
2024-12-10 00:39:48 +0530

    removing clerk provider

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,6 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import {
-  ClerkProvider,
-  SignInButton,
-  SignedIn,
-  SignedOut,
-  UserButton,
-} from "@clerk/nextjs";
 import QueryProviders from "@/providers/query-provider";
 import { SheetProvider } from "@/providers/sheet-provider";
 import { Toaster } from "@/components/ui/sonner";
@@ -16,7 +9,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "finance.io",
-  description: ""
+  description: "",
 };
 
 export default function RootLayout({
@@ -25,17 +18,14 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <ClerkProvider>
-      <html lang="en">
-        <body className={inter.className}>
-          <QueryProviders>
-            <SheetProvider/>
-            <Toaster/>
-            {children}
-          </QueryProviders>
-        </body>
-      </html>
-      {/* /*TODO*/ }
-    </ClerkProvider>
+    <html lang="en">
+      <body className={inter.className}>
+        <QueryProviders>
+          <SheetProvider />
+          <Toaster />
+          {children}
+        </QueryProviders>
+      </body>
+    </html>
   );
 }
